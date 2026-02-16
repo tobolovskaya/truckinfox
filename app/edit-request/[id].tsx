d32e2d60b84df3dce4fcc9d542a29115577425b5
tobolovskaya <tobolovskaya@gmail.com>
2026-02-16 16:52:19 +0100

    fix: Remove invisible character from import statement in EditRequestScreen

--- app/edit-request/[id].tsx
+++ app/edit-request/[id].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+﻿import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
