73d27ae224d9ba0eb3885cb591b5fe84de0b6ac8
tobolovskaya <tobolovskaya@gmail.com>
2026-02-17 17:07:04 +0100

    refactor: remove unused import from Toast component

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Animated, StyleSheet, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { colors, spacing, fontSize, fontWeight, borderRadius } from '../lib/sharedStyles';
 
