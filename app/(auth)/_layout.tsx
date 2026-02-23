49063d36dd758305d86ac6955dd8244d0dce7d6c
tobolovskaya <tobolovskaya@gmail.com>
2026-02-23 23:10:53 +0100

    refactor: remove sign-in screen and update layout to use login instead

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,7 +4,6 @@ export default function AuthLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" />
-      <Stack.Screen name="sign-in" />
       <Stack.Screen name="register" />
       <Stack.Screen name="forgot-password" />
       <Stack.Screen name="logg-ut" />
